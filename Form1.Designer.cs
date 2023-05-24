789d1bff02ca7ce95e6cb6b46d8052e1217ae61e
Harshal Abhiman Thakare <99874760+HarshalThakare8055@users.noreply.github.com>
2023-05-24 10:04:18 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace RainBow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chbRed = new System.Windows.Forms.CheckBox();
+            this.chbOrange = new System.Windows.Forms.CheckBox();
+            this.Pnl0 = new System.Windows.Forms.Panel();
+            this.Pnl1 = new System.Windows.Forms.Panel();
+            this.Pnl3 = new System.Windows.Forms.Panel();
+            this.Pnl2 = new System.Windows.Forms.Panel();
+            this.chbGreen = new System.Windows.Forms.CheckBox();
+            this.chbYellow = new System.Windows.Forms.CheckBox();
+            this.Pnl5 = new System.Windows.Forms.Panel();
+            this.Pnl4 = new System.Windows.Forms.Panel();
+            this.chbIndigo = new System.Windows.Forms.CheckBox();
+            this.chbBlue = new System.Windows.Forms.CheckBox();
+            this.Pnl6 = new System.Windows.Forms.Panel();
+            this.chbViolet = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chbRed
+            // 
+            this.chbRed.AutoSize = true;
+            this.chbRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbRed.Location = new System.Drawing.Point(294, 131);
+            this.chbRed.Name = "chbRed";
+            this.chbRed.Size = new System.Drawing.Size(67, 28);
+            this.chbRed.TabIndex = 0;
+            this.chbRed.Text = "Red";
+            this.chbRed.UseVisualStyleBackColor = true;
+            this.chbRed.CheckedChanged += new System.EventHandler(this.chbColor_CheckedChanged);
+            // 
+            // chbOrange
+            // 
+            this.chbOrange.AutoSize = true;
+            this.chbOrange.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbOrange.Location = new System.Drawing.Point(294, 165);
+            this.chbOrange.Name = "chbOrange";
+            this.chbOrange.Size = new System.Drawing.Size(99, 28);
+            this.chbOrange.TabIndex = 1;
+            this.chbOrange.Text = "Orange";
+            this.chbOrange.UseVisualStyleBackColor = true;
+            this.chbOrange.CheckedChanged += new System.EventHandler(this.chbColor_CheckedChanged);
+            // 
+            // Pnl0
+            // 
+            this.Pnl0.Location = new System.Drawing.Point(417, 123);
+            this.Pnl0.Name = "Pnl0";
+            this.Pnl0.Size = new System.Drawing.Size(711, 46);
+            this.Pnl0.TabIndex = 2;
+            // 
+            // Pnl1
+            // 
+            this.Pnl1.Location = new System.Drawing.Point(417, 165);
+            this.Pnl1.Name = "Pnl1";
+            this.Pnl1.Size = new System.Drawing.Size(711, 38);
+            this.Pnl1.TabIndex = 3;
+            // 
+            // Pnl3
+            // 
+            this.Pnl3.Location = new System.Drawing.Point(417, 236);
+            this.Pnl3.Name = "Pnl3";
+            this.Pnl3.Size = new System.Drawing.Size(711, 31);
+            this.Pnl3.TabIndex = 7;
+            // 
+            // Pnl2
+            // 
+            this.Pnl2.Location = new System.Drawing.Point(417, 199);
+            this.Pnl2.Name = "Pnl2";
+            this.Pnl2.Size = new System.Drawing.Size(711, 41);
+            this.Pnl2.TabIndex = 6;
+            // 
+            // chbGreen
+            // 
+            this.chbGreen.AutoSize = true;
+            this.chbGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbGreen.Location = new System.Drawing.Point(294, 239);
+            this.chbGreen.Name = "chbGreen";
+            this.chbGreen.Size = new System.Drawing.Size(87, 28);
+            this.chbGreen.TabIndex = 5;
+            this.chbGreen.Text = "Green";
+            this.chbGreen.UseVisualStyleBackColor = true;
+            this.chbGreen.CheckedChanged += new System.EventHandler(this.chbColor_CheckedChanged);
+            // 
+            // chbYellow
+            // 
+            this.chbYellow.AutoSize = true;
+            this.chbYellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbYellow.Location = new System.Drawing.Point(294, 202);
+            this.chbYellow.Name = "chbYellow";
+            this.chbYellow.Size = new System.Drawing.Size(91, 28);
+            this.chbYellow.TabIndex = 4;
+            this.chbYellow.Text = "Yellow";
+            this.chbYellow.UseVisualStyleBackColor = true;
+            this.chbYellow.CheckedChanged += new System.EventHandler(this.chbColor_CheckedChanged);
+            // 
+            // Pnl5
+            // 
+            this.Pnl5.Location = new System.Drawing.Point(417, 300);
+            this.Pnl5.Name = "Pnl5";
+            this.Pnl5.Size = new System.Drawing.Size(711, 46);
+            this.Pnl5.TabIndex = 7;
+            // 
+            // Pnl4
+            // 
+            this.Pnl4.Location = new System.Drawing.Point(417, 263);
+            this.Pnl4.Name = "Pnl4";
+            this.Pnl4.Size = new System.Drawing.Size(711, 38);
+            this.Pnl4.TabIndex = 6;
+            // 
+            // chbIndigo
+            // 
+            this.chbIndigo.AutoSize = true;
+            this.chbIndigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbIndigo.Location = new System.Drawing.Point(294, 313);
+            this.chbIndigo.Name = "chbIndigo";
+            this.chbIndigo.Size = new System.Drawing.Size(87, 28);
+            this.chbIndigo.TabIndex = 5;
+            this.chbIndigo.Text = "Indigo";
+            this.chbIndigo.UseVisualStyleBackColor = true;
+            this.chbIndigo.CheckedChanged += new System.EventHandler(this.chbColor_CheckedChanged);
+            // 
+            // chbBlue
+            // 
+            this.chbBlue.AutoSize = true;
+            this.chbBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbBlue.Location = new System.Drawing.Point(294, 276);
+            this.chbBlue.Name = "chbBlue";
+            this.chbBlue.Size = new System.Drawing.Size(71, 28);
+            this.chbBlue.TabIndex = 4;
+            this.chbBlue.Text = "Blue";
+            this.chbBlue.UseVisualStyleBackColor = true;
+            this.chbBlue.CheckedChanged += new System.EventHandler(this.chbColor_CheckedChanged);
+            // 
+            // Pnl6
+            // 
+            this.Pnl6.Location = new System.Drawing.Point(417, 337);
+            this.Pnl6.Name = "Pnl6";
+            this.Pnl6.Size = new System.Drawing.Size(711, 46);
+            this.Pnl6.TabIndex = 9;
+            // 
+            // chbViolet
+            // 
+            this.chbViolet.AutoSize = true;
+            this.chbViolet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbViolet.Location = new System.Drawing.Point(294, 350);
+            this.chbViolet.Name = "chbViolet";
+            this.chbViolet.Size = new System.Drawing.Size(82, 28);
+            this.chbViolet.TabIndex = 8;
+            this.chbViolet.Text = "Violet";
+            this.chbViolet.UseVisualStyleBackColor = true;
+            this.chbViolet.CheckedChanged += new System.EventHandler(this.chbColor_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(526, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(474, 73);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "RainBow Color ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1471, 755);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Pnl6);
+            this.Controls.Add(this.chbViolet);
+            this.Controls.Add(this.Pnl5);
+            this.Controls.Add(this.Pnl3);
+            this.Controls.Add(this.Pnl4);
+            this.Controls.Add(this.chbIndigo);
+            this.Controls.Add(this.Pnl1);
+            this.Controls.Add(this.chbBlue);
+            this.Controls.Add(this.Pnl2);
+            this.Controls.Add(this.chbGreen);
+            this.Controls.Add(this.Pnl0);
+            this.Controls.Add(this.chbYellow);
+            this.Controls.Add(this.chbOrange);
+            this.Controls.Add(this.chbRed);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chbRed;
+        private System.Windows.Forms.CheckBox chbOrange;
+        private System.Windows.Forms.Panel Pnl0;
+        private System.Windows.Forms.Panel Pnl1;
+        private System.Windows.Forms.Panel Pnl3;
+        private System.Windows.Forms.Panel Pnl2;
+        private System.Windows.Forms.CheckBox chbGreen;
+        private System.Windows.Forms.CheckBox chbYellow;
+        private System.Windows.Forms.Panel Pnl5;
+        private System.Windows.Forms.Panel Pnl4;
+        private System.Windows.Forms.CheckBox chbIndigo;
+        private System.Windows.Forms.CheckBox chbBlue;
+        private System.Windows.Forms.Panel Pnl6;
+        private System.Windows.Forms.CheckBox chbViolet;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RainBow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //if(chbRed.Checked==true||chbOrange.Checked==true||chbYellow.Checked==true||chbGreen.Checked==true||chbBlue.Checked==true||chbIndigo.Checked==true||chbViolet.Checked==true)
+            //{
+            //    if (chbRed.Checked == false)
+            //    {
+            //        PnlRed.BackColor = System.Drawing.Color.Transparent;
+            //    }
+            //    else PnlOrange.BackColor = System.Drawing.Color.Orange;
+            //}
+            //else
+            /*if (chbRed.Checked == false)
+            {
+                Pnl0.BackColor = System.Drawing.Color.Orange;
+            }
+            else if (chbOrange.Checked == false)
+            {
+                Pnl1.BackColor = System.Drawing.Color.Yellow;
+            }
+            else if (chbYellow.Checked == true)
+
+            {
+                Pnl2.BackColor = System.Drawing.Color.Yellow;
+            }
+            else if (chbGreen.Checked == true)
+            {
+                Pnl3.BackColor = System.Drawing.Color.Green;
+            }
+            else if (chbBlue.Checked == true)
+            {
+                Pnl4.BackColor = System.Drawing.Color.Blue;
+            }
+            else if (chbIndigo.Checked == true)
+            {
+                Pnl5.BackColor = System.Drawing.Color.Indigo;
+
+            }
+            else if (chbViolet.Checked == true)
+            {
+                Pnl6.BackColor = System.Drawing.Color.Violet;
+            }
+            else
+            {
+                chbRed.Checked = false;
+                chbOrange.Checked = false;
+                chbYellow.Checked = false;
+                chbGreen.Checked = false;
+                chbBlue.Checked = false;
+                chbIndigo.Checked = false;
+                chbViolet.Checked = false;
+            }*/
+        }
+
+        private void FillColors(Color[] colors)//method
+        {
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Panel pnlToColor = GetPanelById(i);
+                if(pnlToColor != null)
+                    pnlToColor.BackColor = colors[i];
+            }
+        }
+
+        private Panel GetPanelById(int id)
+        {
+            Panel pnlToReturn = null;
+            Control[] ctrl = Controls.Find($"Pnl{id}", true);
+            if (ctrl.Length > 0)
+                if (ctrl[0] is Panel)
+                    pnlToReturn = (Panel)ctrl[0];
+            return pnlToReturn;
+        }
+
+        private void chbColor_CheckedChanged(object sender, EventArgs e)
+        {
+            List<Color> rainbowColors = new List<Color>();
+            if (chbRed.Checked)
+                rainbowColors.Add(Color.Red);
+            if(chbOrange.Checked)
+                rainbowColors.Add(Color.Orange);
+            if (chbYellow.Checked)
+                rainbowColors.Add(Color.Yellow);
+            if (chbGreen.Checked)
+                rainbowColors.Add(Color.Green);
+            if (chbBlue.Checked)
+                rainbowColors.Add(Color.Blue);
+            if (chbIndigo.Checked)
+                rainbowColors.Add(Color.Indigo);
+            if (chbViolet.Checked)
+                rainbowColors.Add(Color.Violet);
+            if(rainbowColors.Count < 7)
+            {
+                do
+                {
+                    rainbowColors.Add(Color.Transparent);
+                } while (rainbowColors.Count != 7);
+            }
+            FillColors(rainbowColors.ToArray());
+        }
+
+        private void chbRed_CheckedChanged(object sender, EventArgs e)
+        {
+            /*if (chbRed.Checked == true)
+            {
+                Pnl0.BackColor = System.Drawing.Color.Red;
+            }
+            else
+            
+                Pnl0.BackColor = System.Drawing.Color.Transparent;
+            
+           
+
+            */
+        }
+
+        private void chbOrange_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chbOrange.Checked == true)
+            {
+                Pnl1.BackColor = System.Drawing.Color.Orange;
+            }
+            else Pnl1.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private void chbYellow_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chbGreen_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chbBlue_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chbIndigo_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chbViolet_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
